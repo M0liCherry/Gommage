60d838e91a49d0f1320cfb6e95f5b886356d2bca
M0liCherry <maazbaig2006@gmail.com>
2026-04-22 04:45:21 -0700

    connection test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch("/tasks")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setTasks(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <h1>Tasks</h1>
+      {tasks.map(t => (
+        <p key={t.id}>
+          {t.message} ({t.time})
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ))}
     </div>
   );
 }
